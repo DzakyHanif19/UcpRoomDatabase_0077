25e9c5a171d820b7598c38518677226f0ebecc82
DzakyHanif19 <dzakyannafi1@gmail.com>
2024-12-24 10:51:26 +0700

    menambahkan top app bar

---
+++ app/src/main/java/com/example/ucp2/ui/costumwidget/TopAppBar.kt
@@ -0,0 +1,77 @@
+package com.example.ucp2.ui.costumwidget
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.R
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+) {
+    val backgroundColor = Color(0xFF1976D2)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(backgroundColor)
+            .padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+        if (showBackButton) {
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.align(Alignment.CenterStart).padding(top = 40.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Kembali",
+                    tint = Color.White
+                )
+            }
+        }
+
+        Text(
+            text = judul,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center).padding(top = 40.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .padding(top = 23.dp)
+                .padding(5.dp)
+                .size(50.dp)
+                .background(color = Color.White, shape = CircleShape)
+                .align(Alignment.CenterEnd),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(32.dp)
+            )
+        }
+    }
+}
